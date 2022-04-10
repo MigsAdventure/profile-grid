96a96a1bc9be2b04720ad90fcab94ff1cd325376
MigsAdventure <migsub77@gmail.com>
2022-04-10 16:23:03 -0700

    styled arrows on modal carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import CarouselSlide from './CarouselSlide';
-import { Grid } from '@mui/material';
+import { Grid, styled } from '@mui/material';
+import { Close as CloseIcon, ChevronLeft, ChevronRight } from '@mui/icons-material';
 
 const Carousel = (props) => {
   const Arrow = (props) => {
     const { direction, clickHandler } = props;
-    const icon = direction === 'left' ? <div>left</div> : <div>right</div>
-    return <div onClick={clickHandler}>{icon}</div>
+    const icon = direction === 'left' ? <ChevronLeft fontSize={'large'} />: <ChevronRight fontSize={'large'} />
+    return (<div
+        style={{width: '80px',
+        height: '240px', display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'}}
+        onClick={clickHandler}
+      >
+        {icon}
+      </div>)
   }
   
   const [index, setIndex] = useState(props.currIndex);
@@ -18,18 +27,39 @@ const Carousel = (props) => {
     setIndex(newIndex);
   }
   
+  const StyledGridItem = styled(Grid)(({left}) => (
+    {
+      pointerEvents: 'auto',
+      position: 'fixed',
+      transform: 'translateY(-50%)',
+      top: '50%', [left ? 'left': 'right'] : 0,
+      color: 'white',
+      display: 'flex',
+      transition: '0.3s',
+      '&:hover': {
+        backgroundColor: '#000',
+        opacity: 0.5
+      }
+    }
+  ));
+  
   return (
-    <Grid container sx={{ zIndex: '9999999', display:'flex', alignItems: 'center', justifyContent: 'center' }} >
-      <Grid item xs={1}>
-        <Arrow direction={'left'} clickHandler={() => onArrowClick('left')} />
+    <>
+      {/*Portfolio 4 doesn't have any click events on icon*/}
+      <CloseIcon sx={{ position: 'absolute', top: 0, right: 0, fontSize: 40, fontWeight: 'bold', p: 3, color: 'white', opacity: '0.3', pointerEvents: 'none' }} />
+      <Grid container sx={{ zIndex: '9999999', display:'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none', flexWrap: 'nowrap' }} >
+        <StyledGridItem item left>
+          <Arrow direction={'left'} clickHandler={() => onArrowClick('left')} />
+        </StyledGridItem>
+        {/*Did not use mui breakpoints to match the portfolio 4 exact widths*/}
+        <Grid sx={{pointerEvents: 'auto', width: 'calc(100% - 160px)'}} item>
+          <CarouselSlide  content={props.content[index]}/> )
+        </Grid>
+        <StyledGridItem item right>
+          <Arrow direction={'right'} clickHandler={() => onArrowClick('right')} />
+        </StyledGridItem>
       </Grid>
-      <Grid item xs={10}>
-        <CarouselSlide  content={props.content[index]}/> )
-      </Grid>
-      <Grid item xs={1}>
-        <Arrow direction={'right'} clickHandler={() => onArrowClick('right')} />
-      </Grid>
-    </Grid>
+    </>
   )
 }
 
--- src/components/Carousel/CarouselSlide.js
+++ src/components/Carousel/CarouselSlide.js
@@ -3,18 +3,27 @@ import { Box, Grid, Typography } from "@mui/material";
 
 const CarouselSlide = ({content}) => {
   return (
-    <Box sx={{ flexGrow: 1, backgroundColor: 'white' }}>
-      <Grid container spacing={2}>
+    <Box sx={{ flexGrow: 1, backgroundColor: 'white', mt: 5, zIndex: 9999 }}>
+      <Grid container spacing={2} sx={{ p: 5 }}>
         <Grid item xs={12}>
-          <img src={content.images.webp.image_url} alt=""/>
+          <Typography variant={'h2'} fontWeight={'bold'}>
+            {content.title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <img style={{width: '100%', maxWidth: 450}} src={content.images.webp.large_image_url} alt={content.title}/>
         </Grid>
         <Grid item xs={4}>
-          {/**/}
-        </Grid>
-        <Grid item xs={4}>
-          {/**/}
+          <Typography variant={'h3'}>
+            Test
+          </Typography>
         </Grid>
         <Grid item xs={8}>
+          <Typography  variant={'h3'}>
+            Test
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
           {/**/}
         </Grid>
       </Grid>
